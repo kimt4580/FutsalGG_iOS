cab0878cfdfc62a3ec92a991ed635e4b02e585d4
김태훈 <>
2025-03-20 10:03:04 +0900

    ✨Feat : 디자인에 따른 Color Assets 추가

---
+++ FutsalGG/FutsalGGApp.swift
@@ -0,0 +1,17 @@
+//
+//  FutsalGGApp.swift
+//  FutsalGG
+//
+//  Created by Lukus on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct FutsalGGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
