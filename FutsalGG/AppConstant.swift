559376febf376b9a1b2f25b4868a20bfc0590f11
김태훈 <>
2025-03-25 18:36:02 +0900

    ✨Feat : Base URL과 Dev URL을 저장하는 AppConstant 구현

---
+++ FutsalGG/AppConstant.swift
@@ -0,0 +1,18 @@
+//
+//  AppConstant.swift
+//  FutsalGG
+//
+//  Created by 김태훈 on 3/25/25.
+//
+
+import Foundation
+
+struct AppConstant {
+    static let shared = AppConstant()
+    
+    let BASE_URL = ""
+    let DEV_URL: String = ""
+    
+    let TERM_URL = ""
+    let PRIVICY_URL = ""
+}
