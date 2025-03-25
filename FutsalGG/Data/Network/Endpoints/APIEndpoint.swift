31154bff84d6904041fef05552dc6d7084886669
김태훈 <>
2025-03-25 18:33:09 +0900

    ✨Feat : 공통으로 사용되는 Endpoint와 인증 관련 Endpoint 구현

---
+++ FutsalGG/Data/Network/Endpoints/APIEndpoint.swift
@@ -0,0 +1,47 @@
+//
+//  APIEndpoint.swift
+//  FutsalGG
+//
+//  Created by 김태훈 on 3/25/25.
+//
+
+import Foundation
+import Moya
+
+protocol APIEndpoint: TargetType {
+    var requiresAuth: Bool { get }
+    var customHeaders: [String: String]? { get }  // 추가
+}
+
+extension APIEndpoint {
+    var baseURL: URL {
+        #if DEBUG
+        return URL(string: AppConstant.shared.DEV_URL)!
+        #else
+        return URL(string: AppConstant.shared.BASE_URL)!
+        #endif
+    }
+    
+    // 기본 헤더 구현
+    var headers: [String: String]? {
+        var headers = ["Content-Type": "application/json"]
+        
+        // 커스텀 헤더가 있으면 추가
+        if let customHeaders = customHeaders {
+            headers.merge(customHeaders) { current, _ in current }
+        }
+        
+        // 인증이 필요하면 Bearer 토큰 추가
+        if requiresAuth,
+           let token = TokenManager.shared.accessToken {
+            headers["Authorization"] = "Bearer \(token)"
+        }
+        
+        return headers
+    }
+    
+    // 기본값 제공
+    var customHeaders: [String: String]? { nil }
+    
+    var sampleData: Data { Data() }
+}
---
+++ FutsalGG/Data/Network/Endpoints/AuthEndpoint.swift
@@ -0,0 +1,55 @@
+//
+//  AuthEndpoint.swift
+//  FutsalGG
+//
+//  Created by 김태훈 on 3/25/25.
+//
+
+import Foundation
+import Moya
+
+enum AuthEndpoint {
+    case login(LoginRequestDTO)
+    case refresh
+}
+
+extension AuthEndpoint: APIEndpoint {
+    var path: String {
+        switch self {
+        case .login:
+            return "/auth/login"
+        case .refresh:
+            return "/auth/refresh"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .login, .refresh:
+            return .post
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case .login(let request):
+            return .requestJSONEncodable(request)
+        case .refresh:
+            return .requestPlain
+        }
+    }
+    
+    var requiresAuth: Bool { false }
+    
+    var customHeaders: [String: String]? {
+        switch self {
+        case .refresh:
+            if let refreshToken = TokenManager.shared.refreshToken {
+                return ["Authorization": "Bearer \(refreshToken)"]
+            }
+            return nil
+        default:
+            return nil
+        }
+    }
+}
