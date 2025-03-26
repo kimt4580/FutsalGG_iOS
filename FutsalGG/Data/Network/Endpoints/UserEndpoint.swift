e11ba0ec39ded83f225f96a90355fc0ae00275a9
김태훈 <>
2025-03-26 13:06:41 +0900

    ✨Feat : 회원가입과 닉네임 중복 검색 Endpoint 구현

--- FutsalGG/Data/Network/Endpoints/UserEndpoint.swift
+++ FutsalGG/Data/Network/Endpoints/UserEndpoint.swift
@@ -9,5 +9,41 @@ import Foundation
 import Moya
 
 enum UserEndpoint {
-    case signUp
+    case signUp(_ signUpRequest: SignUpRequestDTO)
+    case checkNickname(_ nickname: String)
+}
+
+extension UserEndpoint: APIEndpoint {
+    var requiresAuth: Bool {
+        true
+    }
+    
+    var path: String {
+        switch self {
+        case .signUp:
+            return "/users"
+        case .checkNickname:
+            return "/users/check-nickname"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .signUp:
+            return .patch
+        case .checkNickname:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .signUp(let request):
+            return .requestJSONEncodable(request)
+        case .checkNickname(let nickname):
+            return .requestParameters(parameters: ["nickname" : nickname], encoding: URLEncoding.queryString)
+        }
+    }
+    
+    
 }
